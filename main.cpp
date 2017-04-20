e962789bb472d0705c83a8f6b8e758e7dcbad498
Guilherme Iecker <guilherme@super.land.ufrj.br>
2017-04-20 10:49:51 -0300

    all logic implemented

--- main.cpp
+++ main.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
-#include "bigInt.h"
+#include "BigInt.h"
 
 using namespace std;
 
 int main()
 {
-  BigInt n1(256);
-  BigInt n2(128);
+  BigInt x((uint64_t)0);
+  BigInt y((uint128_t)1);
 
-  n1 = (uint128_t) 500;
-  n2 = (uint128_t) 2;
+  if (x==y)
+    cout << "== esta errado" << endl;
+  else
+    cout << "x == y" << endl;
 
-  n1 = ~n1;
-  n2 = ~n2;
+  BigInt z((uint64_t)0);
+  z = x + y;
+  cout << "z=" << z << endl;
 
-  cout << (n2 < n1);
+  z--;
+  cout << "z--=" << z << endl;
+
+  z = ~z;
+  cout << "~z =" << z << endl;
+
+  z = z & x;
+  cout << "z&0=" << z << endl;
+
+  z++;
+  cout << "z++=" << z << endl;
+
+  z = z << 4;
+  cout << "z<<=" << z << endl;
+
+  z = z >> 1;
+  cout << "z>>=" << z << endl;
 
   return 0;
 }
